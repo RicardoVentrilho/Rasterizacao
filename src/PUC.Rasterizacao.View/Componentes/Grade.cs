2327541067feef04663c08b69b76f8f140401f5c
José Ricardo <ricardoventrilho@gmail.com>
2017-10-27 13:09:49 -0200

    Criando linhas na grade.

--- src/PUC.Rasterizacao.View/Componentes/Grade.cs
+++ src/PUC.Rasterizacao.View/Componentes/Grade.cs
@@ -1,35 +1,51 @@
-﻿using System;
+﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace PUC.Rasterizacao.View.Componentes
 {
     public class Grade : Canvas
     {
+        private int _Proporcao;
         private bool _ComLinhas;
 
         #region "PROPRIEDADES"
 
+        public int Proporcao
+        {
+            get { return _Proporcao; }
+            set { _Proporcao = value; }
+        }
+
         public int Tamanho
         {
-            get => (int)Height;
-            set => Height = Width = value;
+            get
+            {
+                return (int)Height;
+            }
+            set
+            {
+                Height = Width = value;
+            }
         }
 
         public bool PinteLinhas
         {
-            get => _ComLinhas;
+            get { return _ComLinhas; }
             set
             {
                 _ComLinhas = value;
 
                 if (_ComLinhas)
                 {
-                    int quantidadeDeLinhas = Tamanho / 10;
+                    int quantidadeDeLinhas = Tamanho / _Proporcao;
 
-                    for (int i = 0; i < quantidadeDeLinhas; i++)
+
+                    for (int i = 0; i <= quantidadeDeLinhas; i++)
                     {
-                        AdicionaLinhaVertical(i);
-                        AdicionaLinhaHorizontal(i);
+                        AdicionaLinhaHorizontal(i * _Proporcao);
+                        AdicionaLinhaVertical(i * _Proporcao);
                     }
                 }
             }
@@ -39,12 +55,35 @@ namespace PUC.Rasterizacao.View.Componentes
 
         private void AdicionaLinhaHorizontal(int posicao)
         {
-            throw new NotImplementedException();
+            var linha = new Line
+            {
+                Y1 = posicao,
+                Y2 = posicao,
+                X1 = 0,
+                X2 = Tamanho,
+                Stroke = Brushes.Gray
+            };
+
+            AdicionaElementoFilho(linha);
         }
 
         private void AdicionaLinhaVertical(int posicao)
         {
-            throw new NotImplementedException();
+            var linha = new Line
+            {
+                Y1 = 0,
+                Y2 = Tamanho,
+                X1 = posicao,
+                X2 = posicao,
+                Stroke = Brushes.Gray
+            };
+
+            AdicionaElementoFilho(linha);
+        }
+
+        private void AdicionaElementoFilho(UIElement elemento)
+        {
+            Children.Add(elemento);
         }
     }
 }
