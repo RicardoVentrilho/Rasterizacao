10d52fb5bf3b6401e9fae20ed083b2cbe6bc7ae7
Ricardo Ventrilho <ricardoventrilho@gmail.com>
2017-10-27 00:51:49 -0200

    Add project files.

---
+++ src/PUC.Rasterizacao.Controller/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUC.Rasterizacao.Controller
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/PUC.Rasterizacao.Model/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace PUC.Rasterizacao.Model
+{
+    public class Class1
+    {
+    }
+}
